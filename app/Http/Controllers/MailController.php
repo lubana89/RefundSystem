37a85356bc9ccf6e6f2892157849e8604c4e2dbd
Jaspreet Singh <jaashy.singh@gmail.com>
2016-09-21 10:04:28 +0800

    send mails on link generation

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class MailController extends Controller
+{
+
+
+    public function Email($Subject, $Body)
+    {
+        if($Subject=='Refund Link'){
+        Mail::send('emails.RefundLink', array('link' => $Body, 'name' => Auth::user()->name), function ($message) use ($Subject) {
+            $message->to(Auth::user()->email, Auth::user()->name)->subject
+            ($Subject);
+            $message->from('no-reply@oubo.de', 'OUBO');
+        });
+        }
+        return;
+    }
+
+
+}
---
+++ resources/views/emails/RefundLink.blade.php
@@ -0,0 +1,29 @@
+
+<style>
+
+    .btn {
+        background-color: #f44336;
+        color: #ffffff;
+        display: inline-block;
+        font-size: 16px;
+        margin-left: 20px;
+        opacity: 0.9;
+        padding: 14px 25px;
+        text-align: center;
+        text-decoration: none;
+    }
+</style>
+Dear {!! $name !!},
+<br/>
+<br/>
+Following is the Refund Link for the Item You have Requested.
+<br/>
+<br/>
+<a class="btn" href={!! $link !!} target="_blank">Refund Item Link</a>
+
+<br/>
+<br/>
+Regards
+<br/>
+Team OUBO
+
